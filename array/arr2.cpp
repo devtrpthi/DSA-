563128a76060ffc6857f87f7076c9bcf0dceee29
devesh <deveshkrishnatripathi@gmail.com>
2022-09-20 22:31:35 +0530

    updated

--- array/arr2.cpp
+++ array/arr2.cpp
@@ -20,8 +20,14 @@ void printArray(){
     //print the array
   int n = 15;
   for (int i = 0; i < n; i++) {
-   cout << third[i] << " " ; 
+   cout << third[i] << " " ;
   }
+    int fourth[10] = {1};
+  int m =10;
+  for (int  i = 0; i < m; i++) {
+   std::cout << fourth[i] << std::endl; 
+  }
+  
     return 0;
   }
 
