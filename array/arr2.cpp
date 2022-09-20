9bb222ae2b3cf6d1189fd1fa66ae9a525d942f3b
devesh <deveshkrishnatripathi@gmail.com>
2022-09-20 22:24:43 +0530

    accessing entire array

--- array/arr2.cpp
+++ array/arr2.cpp
@@ -15,6 +15,13 @@ void printArray(){
   // initializing an array
       int second[3] ={5 ,7, 11};
       cout << "value at 2 index " << second[2] <<endl;
+
+  int third[15] = {2,7};
+    //print the array
+  int n = 15;
+  for (int i = 0; i < n; i++) {
+   cout << third[i] << " " ; 
+  }
     return 0;
   }
 
