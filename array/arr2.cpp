0cf1d6149558439d19664e8419f6830ce3fa10bc
devesh <deveshkrishnatripathi@gmail.com>
2022-09-20 22:16:27 +0530

    array-basics

---
+++ array/arr2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void printArray(){
+
+}
+  int  main ()
+  {
+   //declare
+    int number[15] ;
+
+    cout << "Value at 1 index" << number[1] << endl;
+    cout << endl << "brst" << endl;
+
+  // initializing an array
+      int second[3] ={5 ,7, 11};
+      cout << "value at 2 index " << second[2] <<endl;
+    return 0;
+  }
+
+
+
