c7833d64e28954d6c393b9dca0923e948d7aaaad
devesh <deveshkrishnatripathi@gmail.com>
2022-09-21 11:25:41 +0530

    bool-found

--- array/linearsearch.cpp
+++ array/linearsearch.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
 
+bool search(int arr[],int size,int key)
+{
+  for (int i = 0; i < size; i++) {
+   if (arr[i] == key)
+   {
+   return 1; 
+   } 
+  }
+ return 0;
+}
 
 int main ()
 {
-  int arr[10] = 
+  int arr[10] ={5,7,-2,10,22,-2,0,5,22,1} ;
+
+  //whether 1 present or not
+  std::cout << "enter the number to be searched" << std::endl; 
+  int key;
+  cin >> key;
+
+  bool found = search(arr, 10, key);
+
+
+
+
   return 0;
 }
