59a21b5701cc720f737d85d75aa4900065b125b2
devesh <deveshkrishnatripathi@gmail.com>
2022-09-21 10:02:49 +0530

    modified

--- array/scope.cpp
+++ array/scope.cpp
@@ -1,25 +1,4 @@
 #include<iostream>
 using namespace std;
 
-void update(int arr[], int n)
-{
-  std::cout << "inside the function" << std::endl;
- // updating array's first element 
-  arr[0] =120;
 
-  for (int i = 0; i < 3; i++) {
-    cout << arr[i] << " ";
-  }  
-
-  std::cout << "end of the function" << std::endl;
-}
-int main ()
-{
-  int arr[3] = {1,2,3};
-  update(arr,3);
-
-  for (int i = 0; i < 3; i++) {
-    cout << arr[i] << " ";
-  }
-  return 0;
-}
