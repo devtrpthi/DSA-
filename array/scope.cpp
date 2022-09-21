3bc787ff461dca63ced2cdd7043a42350b445be9
devesh <deveshkrishnatripathi@gmail.com>
2022-09-21 10:55:46 +0530

    modified

--- array/scope.cpp
+++ array/scope.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int sumarray(int arr[], int n){
+  int s=0;
+  for (int i = 0; i < n; i++) {
+   s = s +arr[i];
+  }
+ return s;
+}
+
 int  main ()
 {
-  int n;
+ int a[100] , int n;
   std::cout << "enter the size of array" << std::endl;
   cin >>  n ;
  std::cout << "ended" << std::endl; 
