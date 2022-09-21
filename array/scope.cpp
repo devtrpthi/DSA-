f2fe2908f22a2f00d55b668234f58ae20415e21f
devesh <deveshkrishnatripathi@gmail.com>
2022-09-21 10:14:43 +0530

    modified

--- array/scope.cpp
+++ array/scope.cpp
@@ -1,4 +1,11 @@
 #include<iostream>
 using namespace std;
 
-
+int  main ()
+{
+  int n;
+  std::cout << "enter the size of array" << std::endl;
+  cin >>  n ;
+ std::cout << "ended" << std::endl; 
+  return 0;
+}
