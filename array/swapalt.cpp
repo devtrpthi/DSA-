9a2dec39598de4adffc1d1319a212b20f2732848
devesh <deveshkrishnatripathi@gmail.com>
2022-09-22 20:48:54 +0530

    modified

--- array/swapalt.cpp
+++ array/swapalt.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 void printArray(int arr[], int n){
   for (int i = 0; i < n; i++) {
-   std::cout << arr[i] << " " << std::endl; 
-  }
+   std::cout << arr[i] << " "; 
+  } std::cout << std::endl;
 }
 
 
 
 void swapalternate(int arr[], int size) {
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < size; i+=2) {
    if (i+1 < size)
    {
     swap(arr[i], arr[i+1]); 
@@ -22,5 +22,8 @@ int main ()
 {
   int even[8] = {5,2,9,4,7,6,1,0};
   int odd[5] = {11,33,9,76,43};
+  swapalternate(even,10);
+  printArray(even,10);
+
   return 0;
 }
